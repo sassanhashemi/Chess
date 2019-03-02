4231867f622a9d65e8cf3d5b951c433c5696548f
Sassan Hashemi <sassan@gmail.com>
2019-03-02 15:22:47 -0800

    updated

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask, render_template, request, session, redirect, url_for
+
+
+app = Flask(__name__)
+
+
+app.secret_key = "development-key"
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
